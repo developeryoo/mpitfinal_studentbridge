52bf040ac9dc834ab021031e753bae765cea8ae2
developeryoo <77091347+developeryoo@users.noreply.github.com>
2023-03-24 05:50:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php 
+    session_start();
+    if (!$_SESSION['user']) {
+        header('Location: /ustumsuufinal/login.php');
+    }
 	$conn = mysqli_connect('localhost', 'root','', 'studentbridge');
 ?>
 <!DOCTYPE html>
@@ -28,7 +32,8 @@
             </nav>
 
             <button class="header__button">
-                <a href="login.php">Логин</a>
+                <a href="/ustumsuufinal/vendor/login.php"><?= $_SESSION['user']['login'] ?></a>
+                <a href="/ustumsuufinal/vendor/logout.php" class="logout">Выход</a>
             </button>
         </header>
     </div>
@@ -196,7 +201,8 @@
 
 
 
-
+    <script src="/ustumsuufinal/public/jquery-3.6.4.min.js"></script>
+    <script src="/ustumsuufinal/public/main.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
